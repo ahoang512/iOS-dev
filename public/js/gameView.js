f667f86c80a169e22b6fcb816c2e72d8c10e9e88
Anthony Hoang <ant.hoang93@gmail.com>
2015-11-12 13:26:10 -0800

    Create the ground for the game

--- public/js/gameView.js
+++ public/js/gameView.js
@@ -8,6 +8,14 @@
     this.canvas = canvas;
     this.canvas.width = game.DIM_X;
     this.canvas.height = game.DIM_Y;
+    this.setupMap();
+
+  };
+
+  GameView.prototype.setupMap = function () {
+    var ctx = this.canvas.getContext("2d");
+    ctx.fillStyle = "#008080";
+    ctx.fillRect(0, this.game.DIM_Y - 100, this.game.DIM_X, 100);
   };
 
 })(this);
